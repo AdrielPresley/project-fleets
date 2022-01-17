c5889b3d5a1c12d74d6770e698be5a69a7c9062e
AdrielPresley <adriel_tel11@hotmail.com>
2022-01-17 15:06:07 -0300

    Adicionar arquivos de projeto.

---
+++ Localiza-Frotas/Startup.cs
@@ -0,0 +1,84 @@
+﻿using Localiza_Frotas.Domain;
+using Localiza_Frotas.Infra.EF;
+using Localiza_Frotas.Infra.Facade;
+using Localiza_Frotas.Infra.Repository;
+using Localiza_Frotas.Infra.Singleton;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+
+namespace Localiza_Frotas
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Localiza_Frotas",
+                    Description = "API - Frotas",
+                    Version = "v1"
+                });
+
+                var apiPath = Path.Combine(AppContext.BaseDirectory, "Localiza_Frotas.xml");
+                c.IncludeXmlComments(apiPath);
+            });
+
+            services.AddTransient<IVeiculoRepository, FrotaRepository>();
+            services.AddTransient<IVeiculoDetran, VeiculoDetranFacade>();
+
+            services.AddSingleton<SingletonContainer>();
+            services.AddDbContext<FrotaContext>(opt =>
+                                               opt.UseInMemoryDatabase("Frota"));
+
+            services.AddHttpClient();
+
+            services.Configure<DetranOptions>(Configuration.GetSection("DetranOptions"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Localiza_Frotas");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
